71021e512ba77f3553d5921775b5feedce3140d0
mohammed gouse <gousemohammed163@gmail.com>
2025-11-23 12:42:07 +0530

    add solution for Q2 – arithmetic operations on two numbers

---
+++ Python/Lecture-01/q2_arithmetic_operations.py.py
@@ -0,0 +1,14 @@
+# Take two numbers as input from the user and print their sum, difference, product, and quotient
+
+num_1 = int(input("Enter First number : "))
+num_2 = int(input("Enter Second number : "))
+
+sum = num_1 + num_2
+difference = num_1 - num_2
+product = num_1 * num_2
+quotient = num_1 / num_2
+
+print(sum)
+print(difference)
+print(product)
+print(quotient)
