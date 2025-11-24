50e06bdee40950302e6e74dbd5319da506a73060
mohammed gouse <gousemohammed163@gmail.com>
2025-11-24 07:24:50 +0530

    Add average of two integers and one float

---
+++ Python/Lecture-01/q3_average_of_two_integers_and_one_float.py
@@ -0,0 +1,9 @@
+# Ask the user to enter two integers ans one float. Convert them all to floats and print their average.
+
+num_1 = int(input("Enter one integer number : "))
+num_2 = int(input("Enter another integer number : "))
+num_3 = float(input("Enter one decimal/float number : "))
+
+average = (num_1 + num_2 + num_3) / 3
+
+print(average)
