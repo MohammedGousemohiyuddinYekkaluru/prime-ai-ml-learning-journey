4c5f7bb1cffa6b6f1a06db78975f23fdb5850ed1
mohammed gouse <gousemohammed163@gmail.com>
2025-11-24 07:38:13 +0530

    Evaluate expression on basis of operator preference

---
+++ Python/Lecture-01/q5_Operator_preference.py
@@ -0,0 +1,12 @@
+#Evaluate and print the result of the following expression:
+
+x = 10 + 3 * 2 ** 2 #22
+
+# according to operator preference it'll be
+# 1) 2 ** 2 = 4
+# 2) 3 * 4 = 12
+# 3) 10 + 12 = 22
+# Ans = 22
+
+print(x)
+
