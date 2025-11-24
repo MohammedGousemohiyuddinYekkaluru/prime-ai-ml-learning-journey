01368c41be50ef404e8782ab0255e885ae9f0f67
mohammed gouse <gousemohammed163@gmail.com>
2025-11-24 07:31:25 +0530

    Print values with their types

---
+++ Python/Lecture-01/q4_print_typeOf_values.py
@@ -0,0 +1,12 @@
+# The user enters a string conataining a number. convert it to integer, float and print all three values with their types.
+
+
+user_num = "45"
+
+int_num = int(user_num)
+float_num = float(user_num)
+str_num = str(user_num)
+
+print(int_num, type(int_num))
+print(float_num, type(float_num))
+print(str_num, type(str_num))
