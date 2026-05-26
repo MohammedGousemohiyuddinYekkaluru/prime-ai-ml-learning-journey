ea1172f2ddc46b02a836a2df710ed0d4c86cdb85
mohammed gouse <gousemohammed163@gmail.com>
2026-05-26 13:12:36 +0530

    Handle form using request method and GET, POST methods

--- flask/main.py
+++ flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request 
 
 app = Flask(__name__, static_folder="assets")
 
@@ -9,6 +9,14 @@ def hello_world():
 @app.route("/login")
 def login_page():
     return render_template("login.html")
+
+@app.route("/handle-login", methods=["GET", "POST"])
+def handle_login():
+    if request.method == "POST":
+        name = request.form["username"]
+        password = request.form["password"]
+
+        return f"Welcome {name}!"
 
 if __name__ == "__main__":
     app.run(debug=True)
